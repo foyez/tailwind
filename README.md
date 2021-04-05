4e9156729570f09ecae11328316f9d2a3ebef8ef
foyez <foyezar@gmail.com>
2021-04-06 01:13:07 +0600

    add README.md & default config file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tailwind CSS
+
+## Tailwind configuration command with PostCSS
+
+```bash
+npx tailwindcss init -p
+```
+
+## Generate default config file
+
+```bash
+npx tailwindcss init demo/tailwind-default-theme.config.js --full
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
+  purge: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        cyan: colors.cyan
-      }
+        cyan: colors.cyan,
+        sick: '#ff0000',
+      },
+      fontSize: {
+        '4.5x1': '2.5rem', // 40px
+      },
     },
   },
   variants: {
